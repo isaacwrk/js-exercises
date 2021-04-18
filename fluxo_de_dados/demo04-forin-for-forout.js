36434949bdd8f143abe61785b9aaac68a0aaa973
isaacwrk <isaacsouzawrk@gmail.com>
2021-04-18 20:06:02 -0300

    forin for forout

---
+++ fluxo_de_dados/demo04-forin-for-forout.js
@@ -0,0 +1,16 @@
+const textoPar = 'par'
+const textoImpar = 'impar'
+
+// for(let index =0; index <= 10; index++){
+//     const decisao = index % 2 === 0 ? textoPar : textoImpar
+//     console.log(`O numero ${index} é ${decisao}`)
+// }
+
+
+const minhaListaDeTarefas = [
+    {
+        id: parseInt(Math.random() * 10),
+        nome: 'Zezinho',
+        superpoder: 'veloz'
+    }
+]
