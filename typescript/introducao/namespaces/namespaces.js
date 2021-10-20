921254e9adc7f69553c7b0dad58fc3236703d4ec
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-20 14:15:58 -0300

    namespaces aninhados

--- typescript/introducao/namespaces/namespaces.js
+++ typescript/introducao/namespaces/namespaces.js
@@ -1,15 +1,19 @@
 "use strict";
-var Areas;
-(function (Areas) {
-    const PI = 3.14;
-    function circunferencia(raio) {
-        return PI * Math.pow(raio, 2);
-    }
-    Areas.circunferencia = circunferencia;
-    function retangulo(base, altura) {
-        return base * altura;
-    }
-    Areas.retangulo = retangulo;
-})(Areas || (Areas = {}));
-console.log(Areas.circunferencia(10));
-console.log(Areas.retangulo(10, 20));
+//namespaces aninhados
+var Geometria;
+(function (Geometria) {
+    let Area;
+    (function (Area) {
+        const PI = 3.14;
+        function circunferencia(raio) {
+            return PI * Math.pow(raio, 2);
+        }
+        Area.circunferencia = circunferencia;
+        function retangulo(base, altura) {
+            return base * altura;
+        }
+        Area.retangulo = retangulo;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
--- typescript/introducao/namespaces/namespaces.ts
+++ typescript/introducao/namespaces/namespaces.ts
@@ -1,4 +1,7 @@
-namespace Areas {
+
+//namespaces aninhados
+namespace Geometria{
+export namespace Area {
 const PI = 3.14
 export function circunferencia(raio:number):number{
     return PI * Math.pow(raio,2)
@@ -8,8 +11,6 @@ export function retangulo(base:number, altura:number):number{
     return base * altura
 }
 }
-
-
-
-console.log(Areas.circunferencia(10))
-console.log(Areas.retangulo(10,20))
+}
+console.log(Geometria.Area.circunferencia(10))
+console.log(Geometria.Area.retangulo(10,20))
