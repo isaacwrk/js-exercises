e23fb21c950137c9360aaad41bfee271d9a66735
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-20 16:24:31 -0300

    interfaces ts

---
+++ typescript/introducao/interfaces/interface.js
@@ -0,0 +1,14 @@
+"use strict";
+function saudarComOla(pessoa) {
+    console.log('Olá ' + pessoa.nome);
+}
+function mudarNome(pessoa) {
+    pessoa.nome = 'Joana';
+}
+const pessoa = {
+    nome: 'João',
+    idade: 27
+};
+saudarComOla(pessoa);
+mudarNome(pessoa);
+saudarComOla(pessoa);
---
+++ typescript/introducao/interfaces/interface.ts
@@ -0,0 +1,20 @@
+interface Humano{
+    nome: String
+}
+
+function saudarComOla(pessoa: Humano){
+    console.log('Olá ' + pessoa.nome)
+}
+
+function mudarNome(pessoa: Humano){
+    pessoa.nome = 'Joana'
+}
+
+const pessoa = {
+    nome: 'João',
+    idade: 27
+}
+
+saudarComOla(pessoa)
+mudarNome(pessoa)
+saudarComOla(pessoa)
