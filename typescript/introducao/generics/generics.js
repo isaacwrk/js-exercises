c97bb4a99d493800c3240d561a35c371c4a79d5b
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-20 21:55:56 -0300

    generics in array

--- typescript/introducao/generics/generics.js
+++ typescript/introducao/generics/generics.js
@@ -6,7 +6,12 @@ function echo(objeto) {
 function echoMelhorado(objeto) {
     return objeto;
 }
-//o generico se adapta ao tipo que eu vou inserir
+//o generico se adapta ao tipo que eu vou inserir, é um tipo definido no momento da execução da função
 console.log(echoMelhorado('João').length);
 console.log(echoMelhorado(27));
 console.log(echoMelhorado({ nome: 'João', idade: 27 }));
+//generics disponiveis na API
+const avaliacoes = [10, 9.3, 7.7];
+avaliacoes.push(8.4);
+//avaliacoes.push('5.5')
+console.log(avaliacoes);
--- typescript/introducao/generics/generics.ts
+++ typescript/introducao/generics/generics.ts
@@ -6,7 +6,13 @@ function echo(objeto:any){
 function echoMelhorado <Generico>(objeto: Generico):Generico{
     return objeto
 }
-//o generico se adapta ao tipo que eu vou inserir
+//o generico se adapta ao tipo que eu vou inserir, é um tipo definido no momento da execução da função
 console.log(echoMelhorado('João').length)
 console.log(echoMelhorado<number>(27))
 console.log(echoMelhorado({nome: 'João', idade:27}))
+
+//generics disponiveis na API
+const avaliacoes : Array<number> = [10,9.3,7.7]
+avaliacoes.push(8.4)
+//avaliacoes.push('5.5')
+console.log(avaliacoes)
