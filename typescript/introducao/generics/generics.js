ccb4952ef2d8f0f29160b83942d8ba206bb71aa0
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-20 22:41:10 -0300

    desafio generics

--- typescript/introducao/generics/generics.js
+++ typescript/introducao/generics/generics.js
@@ -33,3 +33,76 @@ imprimir([
     { nome: 'Zé', idade: 20 },
     { nome: 'Viviane', idade: 27 }
 ]);
+const chamarEcho = echoMelhorado;
+console.log(chamarEcho('algumacoosa'));
+//Classes com genéricos
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+}
+//resolvendo o generico apartir da herança
+class SomaBinaria extends OperacaoBinaria {
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log(new SomaBinaria(3, 4).executar());
+//console.log(new OperacaoBinaria('Bom','Dia').executar())
+//console.log(new OperacaoBinaria(2,2).executar())
+// construindo generico para pegar intervalo entre dias
+class DiffData extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diff = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diff / dia)} dias`;
+    }
+}
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(5, 2, 2020);
+console.log(new DiffData(d1, d2).executar());
+//Desafio Classe Fila Generics
+//atributo: fila Array
+//Metodos : entrar push, proximo splice , imprimir console
+//criando classe generica com um type generico array
+class Fila {
+    constructor(...args) {
+        this.fila = args;
+    }
+    //criando metodos
+    entrar(elemento) {
+        this.fila.push(elemento);
+    }
+    proximo() {
+        if (this.fila.length >= 0 && this.fila[0]) {
+            const primeiro = this.fila[0];
+            this.fila.splice(0, 1);
+            return primeiro;
+        }
+        else {
+            return null;
+        }
+    }
+    imprimir() {
+        console.log(this.fila);
+    }
+}
+//tipando os tipos da classe
+const fila = new Fila('Jose', 'Maria', 'Ana', 'Lu');
+fila.imprimir();
+fila.entrar('luna');
+fila.imprimir();
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
--- typescript/introducao/generics/generics.ts
+++ typescript/introducao/generics/generics.ts
@@ -38,3 +38,87 @@ imprimir<Aluno>([
     {nome:'Zé',idade:20},
     {nome:'Viviane',idade:27}
 ])
+
+//tipo genérico
+type Echo =  <T>(data: T)=>T
+const chamarEcho: Echo = echoMelhorado
+console.log(chamarEcho<string>('algumacoosa'))
+
+//Classes com genéricos
+abstract class OperacaoBinaria<T, R>{
+    constructor(public operando1:T, public operando2:T){}
+    abstract executar(): R
+}
+
+//resolvendo o generico apartir da herança
+class SomaBinaria extends OperacaoBinaria<number,number>{
+    executar():number{
+        return this.operando1 + this.operando2
+    }
+}
+console.log(new SomaBinaria(3,4).executar())
+//console.log(new OperacaoBinaria('Bom','Dia').executar())
+//console.log(new OperacaoBinaria(2,2).executar())
+
+// construindo generico para pegar intervalo entre dias
+class DiffData extends OperacaoBinaria<Data, String>{
+    getTime(data:Data):number{
+        let {dia, mes, ano} = data
+        return new Date(`${mes}/${dia}/${ano}`).getTime()
+    }
+    executar():string{
+        const t1 = this.getTime(this.operando1)
+        const t2 = this.getTime(this.operando2)
+        const diff = Math.abs(t1-t2)
+        const dia = 1000 * 60 * 60 * 24
+        return `${Math.ceil(diff / dia)} dias`
+    }
+}
+
+const d1 = new Data(1,2,2020)
+const d2 = new Data(5,2,2020)
+console.log(new DiffData(d1,d2).executar())
+
+//Desafio Classe Fila Generics
+//atributo: fila Array
+//Metodos : entrar push, proximo splice , imprimir console
+
+//criando classe generica com um type generico array
+class Fila <T> {
+    private fila: Array<T>
+    constructor(...args: T[]){
+        this.fila = args
+    }
+
+    //criando metodos
+    entrar(elemento: T){
+        this.fila.push(elemento)
+    }
+
+    proximo():T | null{
+        if(this.fila.length >= 0 && this.fila[0] ){
+        const primeiro = this.fila[0]
+        this.fila.splice(0,1)
+        return primeiro} else{
+            return null
+        }
+    }
+
+    imprimir(){
+        console.log(this.fila)
+    }
+}
+
+//tipando os tipos da classe
+const fila = new Fila<string>('Jose','Maria','Ana','Lu')
+
+fila.imprimir()
+fila.entrar('luna')
+fila.imprimir()
+console.log(fila.proximo())
+console.log(fila.proximo())
+console.log(fila.proximo())
+console.log(fila.proximo())
+console.log(fila.proximo())
+console.log(fila.proximo())
+console.log(fila.proximo())
